776d82d050d2184fab20d1d31981d4acee7d3a9f
Gregory <maklakov.grigory@gmail.com>
2023-02-21 18:30:15 +0100

    delete tippy

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 "use strict";
 
+const $selectButton = document.querySelector(".crypto-select");
+const $selectBlock = document.querySelector(".select-block");
 
-const $selectButton = document.querySelector('.crypto-select');
-const $selectBlock = document.querySelector('.select-block');
+const label = document.querySelector(".crypto-select__wrp");
+const options = Array.from(document.querySelectorAll(".item-select"));
+const itemSelect = document.querySelector(".item-select__wrp");
 
-const label = document.querySelector('.crypto-select__wrp');
-const options = Array.from(document.querySelectorAll('.item-select'));
-
-//замена контента при виборе 
+//замена контента при виборе
 options.forEach((option) => {
-	option.addEventListener('click', () => {
-		label.textContent = option.textContent;
-        $selectBlock.classList.remove('select-block__active');
-	});
+  option.addEventListener("click", (e) => {
+    const clone = e.target.cloneNode(true);
+    label.innerHTML = clone;
+
+    // пока работает это
+    // label.textContent = option.textContent;
+    $selectBlock.classList.remove("select-block__active");
+  });
 });
 
-$selectButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    $selectBlock.classList.toggle('select-block__active');
+$selectButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  $selectBlock.classList.toggle("select-block__active");
 });
 
