3b968bbd9218658d993bb16b452c70d79e851bd6
Gregory <maklakov.grigory@gmail.com>
2023-04-03 13:41:50 +0200

    add click outside func

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import tippy from "tippy.js";
 import "tippy.js/dist/tippy.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // API ========================================================================================================================================================
-
-  //========================================================================================================================================================
-
   // Получаем элементы формы
   const $select = document.getElementById("select");
   const $input = document.querySelector(".input--receive");
@@ -14,11 +10,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const $ticker = document.querySelector(".crypto-select__coin");
   const $selectItems = document.querySelectorAll(".item-select");
 
-  // Добавляем обработчик события на клик к выпадающему списку
-  document.querySelector(".crypto-select").addEventListener("click", () => {
-    // Показываем список криптовалют
-    $select.classList.toggle("select-block__active");
+  $input.addEventListener("click", () => {
+    // Проверяем наличие класса select-block__active на элементе $select
+    if (!$select.classList.contains("select-block__active")) {
+      // Если класса нет, показываем список криптовалют
+      $select.classList.add("select-block__active");
+    } else {
+      // Если класс есть, скрываем список
+      $select.classList.remove("select-block__active");
+    }
   });
+
+  document.addEventListener(
+    "blur",
+    (event) => {
+      const target = event.target;
+      // Если кликнули не на выпадающий список или его потомка, скрываем его
+      if (!target.closest(".crypto-select")) {
+        setTimeout(() => {
+          $select.classList.remove("select-block__active");
+        }, 100); // задержка в 100 миллисекунд
+      }
+    },
+    true
+  );
+
   // Обновляем содержимое элементов формы
   $selectItems.forEach((item) => {
     item.addEventListener("click", () => {
@@ -32,12 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
       $input.placeholder = `0.00 ${$itemTicker}`;
       $image.setAttribute("src", $itemIcon);
       $ticker.textContent = $itemTicker;
-      $select.classList.remove("select-block__active");
     });
   });
 });
 
-//========================================================================================================================================================
+// Tippy ========================================================================
 tippy("#info", {
   content:
     "There is a dynamic blockchain fee, a.k.a. miner fee, on every transfer and we pay this fee on behalf of you to deliver your crypto to your own wallet address!",
